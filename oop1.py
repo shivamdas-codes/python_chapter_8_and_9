7d7ae79b0895ab6de668b288aaf0c3d29ec9fd01
shivam <shivam984861@gmail.com>
2026-01-08 19:29:23 +0530

    new file added

--- oop1.py
+++ oop1.py
@@ -19,22 +19,27 @@ print(c1.color)
 print(c1.brand)
 
 
+
 # example 3: (IMPORTANT)
 class student:
-    # it is a default constructor
-    # if we dont write any constructor then python provides a default constructor
+    college_name = "avanthi institutes"
+    name = "anonymous"  #class attr...........this is used in case any student name is not provided
+
     def __init__(self):
         pass
-    # it is a parameterized constructor
+    # it is a default constructor
+    # if we dont write any constructor then python provides a default constructor
+    
     def __init__(self,name,marks):  # constructor with parameters
-        self.name = name
+        self.name = name    #obj attr
         self.marks = marks
         print("adding new data to datebase")
+    # it is a parameterized constructor
 
 s1 = student("shivam",95)
 print(s1.name,s1.marks)
+print(student.college_name)
 
 s2 = student("shreya",98)
 print(s2.name,s2.marks)
-
-
+print(student.college_name)
