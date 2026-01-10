45b54d73273178ba52bd44bcf70054e849bc5f5e
shivam <shivam984861@gmail.com>
2026-01-10 19:46:30 +0530

    initial changes

---
+++ oops2.py
@@ -0,0 +1,9 @@
+class student:
+
+    def __init__(self,name):
+        self.name = name
+
+s1 = student("shivam")
+print(s1.name)
+del s1.name
+# print(s1.name)  # This will raise an AttributeError since name has been deleted
