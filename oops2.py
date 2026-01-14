5dfabcd89e61f799f4f44f8bb20de119b6c133ef
shivam <shivam984861@gmail.com>
2026-01-14 19:35:21 +0530

    new changes

--- oops2.py
+++ oops2.py
@@ -11,33 +11,53 @@
 
 
 
-#PRIVATE METHOD:
-# without private attribute:
-class account:
-    def __init__(self,acc_no,acc_pass):
-        self.acc_no = acc_no
-        self.acc_pass = acc_pass
+# #PRIVATE METHOD:
+# # without private attribute:
+# class account:
+#     def __init__(self,acc_no,acc_pass):
+#         self.acc_no = acc_no
+#         self.acc_pass = acc_pass
 
-a1 = account("12345","abcd")
-print(a1.acc_no)
-print(a1.acc_pass)
-# in this case it will print the number and password of the account which is wrong because the private method is not implemented
+# a1 = account("12345","abcd")
+# print(a1.acc_no)
+# print(a1.acc_pass)
+# # in this case it will print the number and password of the account which is wrong because the private method is not implemented
 
 
-# with private attribute:
-class account:
-    def __init__(self,acc_no,acc_pass):
-        self.acc_no = acc_no
-        self.__acc_pass = acc_pass  # Private attribute
+# # with private attribute:
+# class account:
+#     def __init__(self,acc_no,acc_pass):
+#         self.acc_no = acc_no
+#         self.__acc_pass = acc_pass  # Private attribute
 
-    def reset(self):
-        print(self.acc_no)
+#     def reset(self):
+#         print(self.acc_no)
 
-a1 = account("12345","abcd")
-print(a1.acc_no)
-print(a1.reset())
-print(a1.acc_pass)
-# in this case it will print the number and password of the account which is wrong because the private method is not implemented
+# a1 = account("12345","abcd")
+# print(a1.acc_no)
+# print(a1.reset())
+# print(a1.acc_pass)
+# # in this case it will print the number and password of the account which is wrong because the private method is not implemented
 
 
 # INHERITANCE example:
+class car:  #base class
+    color = "black"
+    @staticmethod
+    def start():
+        print("car started")
+    @staticmethod
+    def stop():
+        print("car stopped")
+
+class bmwmodel(car):    #inheritance
+    def __init__(self,model):
+        self.model = model
+
+b1 = bmwmodel("X5")
+print(b1.color)
+print(b1.model)
+b1.start()
+
+    
+        
